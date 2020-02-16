47310ec7ee27a008ebbe704fe6647b0c13318143
Egor Dorichev <egordorichev@gmail.com>
2020-02-16 20:50:48 +0300

    Got lit running in the webadd *!

--- html/glue/glue.c
+++ html/glue/glue.c
@@ -1,24 +1,12 @@
 #include <lit/lit.h>
+#include <lit/vm/lit_vm.h>
 #include <lit/vm/lit_chunk.h>
-#include <lit/debug/lit_debug.h>
 
 #include <stdio.h>
 
 int main(int argc, char* argv[]) {
 	LitState* state = lit_new_state();
-
-	LitChunk chunk;
-	lit_init_chunk(&chunk);
-	lit_write_chunk(state, &chunk, OP_CONSTANT, 1);
-	lit_write_chunk(state, &chunk, lit_chunk_add_constant(state, &chunk, 6.9), 1);
-	lit_write_chunk(state, &chunk, OP_RETURN, 2);
-
-	printf("%u bytes allocated\n", state->bytes_allocated);
-	lit_disassemble_chunk(&chunk, "Test Chunk");
-	lit_shrink_chunk(state, &chunk);
-	lit_free_chunk(state, &chunk);
-	printf("%u bytes allocated\n", state->bytes_allocated);
-
+	lit_interpret(state, "web", "print(\"Halla world\")");
 	lit_free_state(state);
 
 	return 0;
--- todo.md
+++ todo.md
@@ -5,3 +5,8 @@
 * resizeable stack
 * allow to require modules using local path from the module path (module path + require_path)
 * make op_closure take a short for the constant instead of a byte
+* /* */ (block comments)
+* switch statement
+* if expression (? :)
+* multi-line strings
+* string interpolation
